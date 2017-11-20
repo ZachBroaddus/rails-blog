dd7ceee6477e2c0b6bbca25c9a9b34fe490c2a8d
Zach Broaddus <zachbroaddus@zachs-mbp.attlocal.net>
2017-11-19 18:04:02 -0600

    create user model and migration. getting ready to implement has_secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119230211) do
+ActiveRecord::Schema.define(version: 20171119235332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20171119230211) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
   end
 
 end
