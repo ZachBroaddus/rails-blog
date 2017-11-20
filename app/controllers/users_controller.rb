1b9a6f9d0a558cfbdc639b62d857e57c02330dfa
Zach Broaddus <zachbroaddus@zachs-mbp.attlocal.net>
2017-11-19 23:33:53 -0600

    add authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        format.html { redirect_to root_path, notice: 'You have successfully signed up!' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,27 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   has_secure_password
 
-  has_many :posts
+ EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :username, presence: true
+
+  validates :password, length: { minimum: 8}
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+
+
+  # this callback will run before saving on create and update
+  before_save :downcase_email
+
+  # this callback will run after creating a new user
+  after_create :successfully_created
+
+
+  private
+    def downcase_email
+      self.email.downcase!
+    end
+
+    def successfully_created
+      puts "Successfully created a new user"
+    end
 
 end
